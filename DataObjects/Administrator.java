737bb8568f9b4de83feaf770d817521b4226ffdd
Theuns Lombard <theunslb@gmail.com>
2022-08-25 12:21:06 +0200

    Added stuff in the objects

--- DataObjects/Administrator.java
+++ DataObjects/Administrator.java
@@ -1,5 +1,8 @@
 package DataObjects;
 
 public class Administrator {
-    
+    int Admin_id;
+    String Admin_name;
+    String password;
+    String contact;
 }
--- DataObjects/Student.java
+++ DataObjects/Student.java
@@ -1,5 +1,13 @@
 package DataObjects;
 
 public class Student {
-    
+    int student_id;
+    String student_name;
+    String student_address;
+    String student_email;
+    String student_password;
+
+    public Boolean CheckPassword (String Password){
+        return Password==student_password;
+    }
 }
