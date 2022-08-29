58c3c359a0db3411dc40c10ca85e4e0470cc6841
CapnAvery <68193106+CapnAvery@users.noreply.github.com>
2022-08-29 10:15:37 +0200

    Added GUI

---
+++ src/main/java/com/project/GUI/MainFrame.java
@@ -0,0 +1,13 @@
+package src.main.java.src.main.java.com.example.accessingdatamysql;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public static void main(String[] args) {
+        Entrance Start = new Entrance();
+        // int n = 1;
+        Start.setVisible(true);
+    }
+}
